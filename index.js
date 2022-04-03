e3fe3977d18fcf85cbb0c04aff7bf4f24c7d2750
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-04-03 15:34:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const orderRoute = require("./routes/order");
 const cors = require("cors");
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
-cont path =require('path');
+const path =require('path');
 
 
 dotenv.config();

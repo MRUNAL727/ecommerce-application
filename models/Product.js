67897f7afae8cd193b2cceaa2fccddff983b99e0
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-04-05 14:35:14 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -14,4 +14,6 @@ const ProductSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Product", ProductSchema);
+const products = mongoose.model("Product", ProductSchema);
+
+export default products;

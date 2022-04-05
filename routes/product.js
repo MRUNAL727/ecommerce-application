6a7f920edb04724032b5114478ed24fb983c87ec
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-04-05 09:29:08 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -75,7 +75,7 @@ router.get("/", async (req, res) => {
       products = await Product.find();
     }
     console.log(products)
-    res.status(200).json(products);
+    res.status(200).json({products});
   } catch (err) {
     res.status(500).json(err);
   }

0572b225e030026b2f9cd69a8f05ee6e72c50a90
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-04-10 09:26:09 +0530

    Update Product.jsx

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -78,7 +78,7 @@ const Product = ({ item }) => {
           <ShoppingCartOutlined />
         </Icon>
         <Icon>
-          <Link to={`/api/products/find/${item._id}`}>
+          <Link to={`/product/${item._id}`}>
           <SearchOutlined />
           </Link>
         </Icon>
